64abafac7a097d2473a9a1a7f155d3b05bdeb512
JorgeMartinez16 <jorgemariomartinez16@gmail.com>
2024-10-18 15:54:39 -0500

    Registra usuarios y devuelve el token al hacer login

---
+++ src/main/java/com/proyecto/san_felipe/security/JwtTokenUtil.java
@@ -0,0 +1,51 @@
+package com.proyecto.san_felipe.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenUtil {
+
+    private final String SECRET_KEY = "secretsecretsecretsecretsecretsecret"; // Cambia por una clave más segura
+    private final long EXPIRATION_TIME = 86400000; // 1 día en milisegundos
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
+    public boolean validateToken(String token, Authentication authentication) {
+        final String username = extractUsername(token);
+        return (username.equals(authentication.getName()) && !isTokenExpired(token));
+    }
+}
