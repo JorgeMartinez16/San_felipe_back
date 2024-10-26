c5268265a843b27ee3bcf3596b3e10481b188746
JorgeMartinez16 <jorgemariomartinez16@gmail.com>
2024-10-26 09:15:23 -0500

    Configurando cors origin

--- src/main/java/com/proyecto/san_felipe/Controllers/AuthController.java
+++ src/main/java/com/proyecto/san_felipe/Controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.proyecto.san_felipe.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -11,8 +13,13 @@ public class AuthController {
     @Autowired
     private SecurityConfig.AuthService authService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/register")
-    public String register(@RequestParam String username, @RequestParam String password) {
+    public String register(@RequestBody Map<String, String> credentials) {
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+        System.out.println(password);
+        System.out.println(username);
         return authService.register(username, password);
     }
 
